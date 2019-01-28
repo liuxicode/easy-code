35578affcbfd916e7912f093aa16b0da261dee26
liuxi <751046424@qq.com>
2019-01-28 20:27:58 +0800

    Initial commit

---
+++ src/test/java/com/liuxi/code/easycode/EasyCodeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.liuxi.code.easycode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyCodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
